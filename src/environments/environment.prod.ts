3930f8a39a02fa9401e6eb2d6a6e0b70c21a9bd8
Obi-Wan Kadogo <prox0591@gmail.com>
2022-10-18 12:03:40 +0300

    untrack environments

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  googleAPIKeyFirebase: 'AIzaSyD2DRYhAoJAdpoIaISescQrlYjR99xB2Dk',
-  firebaseSignUp:
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
-  firebaseSignIn:
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
-};
